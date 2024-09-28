ed1351953c2c746c862cf4918b252b8e7f848998
greycloudss <91027845+greycloudss@users.noreply.github.com>
2024-09-28 12:30:26 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.banker {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.example.banker to javafx.fxml;
+    exports com.example.banker;
+}
